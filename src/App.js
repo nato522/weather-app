4eb4c01a7e63c955dd4d24c1427dc5e7c88f7d24
Renato Pinto <renatopl@Renatos-MacBook-Pro.local>
2019-12-26 20:27:18 +0100

    components and connection with API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+import API_KEY from './data/ApiKey';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
+  render(){
+    return(
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
